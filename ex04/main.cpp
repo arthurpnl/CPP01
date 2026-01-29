0ab825f96e7b439a3b8412eaf061f93007bea6d9
arthurpnl <arthurpnlcontact1@gmail.com>
2026-01-29 16:55:53 +0100

    ex04.replace

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -9,28 +9,40 @@ bool    checkFiles(std::ifstream &inputFile, std::ofstream &outputFile)
     return (0);
 }
 
-std::string findAndReplace(std::string )
+
+void findAndReplace(std::string s1, std::string s2, std::ifstream &inputFile, std::ofstream &outputFile)
+{
+    std::string line;
+
+    while (std::getline(inputFile, line))
+    {
+        size_t pos = line.find(s1);
+        if (pos != std::string::npos)
+            line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+        outputFile << line << std::endl; 
+    }
+}
 
 int main(int argc, char **argv)
 {
     std::string inputFileN;
     std::string outputFileN;
-    if (argc != 4)
+    if (argc != 4 || !argv[2][0])
     {
         std::cout << "Arguments missing." << std::endl;
         return (1);
     }
     inputFileN = argv[1];
-    outputFileN = argv[1] + ".replace";
+    outputFileN = std::string(argv[1]) + ".replace";
     std::ifstream inputFile;
     std::ofstream outputFile;
-    inputFile.open(inputFileN);
-    outputFile.open(outputFileN); 
+    inputFile.open(inputFileN.c_str());
+    outputFile.open(outputFileN.c_str()); 
     if (checkFiles(inputFile, outputFile) == 0)
     {
         std::cout << "Error while openning the files" << std::endl;
         return (1);
     }
-    findAndReplace(inputFileN, argv[2], argv[3], inputFile, outputFile);
-
+    findAndReplace(argv[2], argv[3], inputFile, outputFile);
+    return (0);
 }
